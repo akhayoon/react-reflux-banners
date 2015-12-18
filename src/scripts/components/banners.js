b25219d2acf12199787c979ca6df2540a2da74a8
Hussien K <akhayoon@gmail.com>
2015-12-18 08:55:14 -0800

    added table to component, using CDN for bootstrap and added initial empty store file

--- src/scripts/components/banners.js
+++ src/scripts/components/banners.js
@@ -1,19 +1,44 @@
 /** @jsx React.DOM */
-var React = require('react');
+var React       = require('react');
+var Reflux      = require('reflux');
+//var BannerStore = require('../stores/BannerStore')
+
+// link to react router
+var Link = require('react-router').Link;
+
+// get our initial banners from our store
+function getBanners() {
+  return { banners: 'banners for our store'}
+}
 
 var Banners = React.createClass({
+
+  getInitialState: function() {
+    return getBanners();
+  },
+
   render: function() {
     return (
-      <div className="banner">
-        Hello, world! I am a Banner.
+      <div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Image</th>
+              <th>URL</th>
+              <th>Active?</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
       </div>
-    );
+    )
   }
+
 });
 
-React.render(
-  <Banners />,
-  document.getElementById('app')
-);
+React.render(<Banners />, document.getElementById('app'));
+
 
 module.exports = Banners;
